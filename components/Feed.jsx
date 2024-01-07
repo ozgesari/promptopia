9ccd06797be6ebfb9bdf5665aeda2edc303f8f1f
Ozge Sari <ozgesari@Ozges-MacBook-Pro.local>
2024-01-07 17:02:00 +0000

    create-prompt

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+      <div className="mt-16 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))}
+      </div>
+    );
+  };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState("");
+
+  return (
+    <div className="prompt_card">
+      <div></div>
+    </div>
+  );
 };
 
 export default PromptCard;
